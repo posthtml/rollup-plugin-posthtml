55e14c9f5c4c8fae87ff47fd0613bd56c37c9c68
Ivan Metelev <ivan.metelev@corp.mail.ru>
2021-04-19 15:56:21 +0300

    chore(feature): emit file option

--- test.js
+++ test.js
@@ -14,6 +14,7 @@ const posthtml = require('./index')
 
 const rollup = require('rollup')
 const elements = require('posthtml-custom-elements')
+const minifier = require('posthtml-minifier')
 
 test('should main export return an object with transform() fn', (done) => {
   const plugin = posthtml()
@@ -45,6 +46,45 @@ test('should work as real plugin to rollup', (done) => {
 
   return promise.then((bundle) => {
     const result = bundle.generate({ format: 'iife' })
+
+    test.strictEqual(/var foo = "<div/.test(result.code), true)
+    test.strictEqual(/class=\\"component\\"/.test(result.code), true)
+    test.strictEqual(/class=\\"text\\"/.test(result.code), true)
+    test.strictEqual(/console\.log\(foo\)/.test(result.code), true)
+    done()
+  }, done).catch(done)
+})
+
+test('should emit string', (done) => {
+  const promise = rollup.rollup({
+    entry: 'fixtures/main.js',
+    plugins: [
+      posthtml({
+        plugins: [
+          elements(),
+          minifier({
+            removeComments: true
+          })
+        ],
+        emitFile: false
+      }),
+      (function rollupPluginDumb () {
+        return {
+          name: 'dumb',
+          transform: (code, id) => {
+            return id.endsWith('.html')
+              ? `export default ${JSON.stringify(code)}`
+              : null
+          }
+        }
+      })()
+    ]
+  })
+
+  return promise.then((bundle) => {
+    const result = bundle.generate({format: 'iife', moduleName: 'dumb'})
+
+    test.strictEqual(result.code.includes('<!--'), false)
 
     test.strictEqual(/var foo = "<div/.test(result.code), true)
     test.strictEqual(/class=\\"component\\"/.test(result.code), true)
